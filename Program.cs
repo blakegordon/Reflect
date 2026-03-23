673cea9fc23aafd9ab8d618c16ec2c82a659218a
blakegordon <53873866+blakegordon@users.noreply.github.com>
2026-03-23 16:44:36 -0700

    Security fixes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -8,11 +8,30 @@ namespace Reflect2 {
 		static void Main(string[] args) {
 			if (args.Length >= 1) {
 				try {
-					string targetDir = Path.GetDirectoryName(args[0]);
-					if (!string.IsNullOrEmpty(targetDir)) {
-						AppDomain.CurrentDomain.AppendPrivatePath(targetDir);
+					if (!File.Exists(args[0])) {
+						Console.WriteLine("File does not exist.");
+						Environment.Exit(1);
 					}
-					Assembly a = Assembly.LoadFrom(args[0]);
+					string extension = Path.GetExtension(args[0]).ToLowerInvariant();
+					if (extension != ".dll" && extension != ".exe") {
+						Console.WriteLine("File must be a .dll or .exe assembly.");
+						Environment.Exit(2);
+					}
+					string targetDir = Path.GetDirectoryName(args[0]);
+					AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += (sender, e) => {
+						string depName = e.Name.Split(',')[0];
+						string depPath = Path.Combine(targetDir, depName + ".dll");
+						if (File.Exists(depPath)) {
+							return Assembly.ReflectionOnlyLoadFrom(depPath);
+						} else {
+							try {
+								return Assembly.ReflectionOnlyLoad(e.Name);
+							} catch {
+								return null;
+							}
+						}
+					};
+					Assembly a = Assembly.ReflectionOnlyLoadFrom(args[0]);
 					Console.WriteLine();
 					foreach (Type t in a.GetTypes().Where(f => f.IsClass && !f.Attributes.HasFlag(TypeAttributes.NestedPrivate)).OrderBy(f => f.Name)) {
 						Console.WriteLine(t.Name);
@@ -25,11 +44,15 @@ namespace Reflect2 {
 						}
 					}
 				}
-				catch (Exception ex) when (ex is ArgumentException || ex is IOException || ex is BadImageFormatException || ex is ReflectionTypeLoadException) {
+				catch (Exception ex) when (ex is ArgumentException || ex is IOException || ex is BadImageFormatException || ex is ReflectionTypeLoadException || ex is FileNotFoundException || ex is FileLoadException) {
 					Console.WriteLine(ex.GetType().Name + ": " + ex.Message);
 					Console.WriteLine();
 					Console.WriteLine("\t" + ex.InnerException?.GetType()?.Name + "\t" + ex.InnerException?.Message);
-					Environment.Exit(1);
+					
+					foreach(Exception le in (ex as ReflectionTypeLoadException)?.LoaderExceptions ?? Array.Empty<Exception>()) {
+						Console.WriteLine("\t" + le.GetType().Name + "\t" + le.Message);
+					}
+					Environment.Exit(3);
 				}
 			}
 		}
